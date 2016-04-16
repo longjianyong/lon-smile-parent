4b8a18a0a54f01f474f46615eb7cbc7170c51d34
longjianyong <329916948@qq.com>
2016-04-16 22:02:34 +0800

    fffffffffffffff

--- src/main/java/com/lon/smile/framework/dao/UserInfoMapper.java
+++ src/main/java/com/lon/smile/framework/dao/UserInfoMapper.java
@@ -3,7 +3,15 @@ package com.lon.smile.framework.dao;
 import com.lon.smile.framework.model.base.UserInfo;
 
 public interface UserInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
     int insert(UserInfo record);
 
     int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
 }
